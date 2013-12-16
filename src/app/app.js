d5a78c55c3026024029d9c8f42eddb68c006ae78
Sebastian Kuligowski <sebastian@kuligowski.pl>
2013-12-16 21:19:25 +0100

    Default sorting to mean

--- src/app/app.js
+++ src/app/app.js
@@ -8,6 +8,9 @@ angular.module('myApp', [
 controller('AppCtrl', ['$scope', function($scope) {
 	$scope.table = {A: null, B: null, C: null, D: null};
 	$scope.score = {white: 10, blue: 10};
-	$scope.playerSort = {};
+	$scope.playerSort = {
+		column: 'mean',
+		descending: true
+	};
 }]);
 
